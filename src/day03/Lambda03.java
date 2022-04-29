844fed3994275f2dccc3b977b8f78245b3bb3cef
Busra Boyali <bsrbyl.bus@gmail.com>
2022-04-29 18:29:00 +0300

    29.04.22

--- src/day03/Lambda03.java
+++ src/day03/Lambda03.java
@@ -2,24 +2,33 @@ package day03;
 
 import day01.Lambda01;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Lambda03 {
     public static void main(String[] args) {
-        List<String> menü = new ArrayList<>(Arrays.asList("küşleme", "adana", "trileçe", "havucDilim", "buryan",
-                "yaglama", "kokorec", "arabAşi", "trileçe", "güveç", "trileçe"));
+        List<String> menu = new ArrayList<>(Arrays.asList("küşleme", "adana", "trileçe", "havucDilim", "buryan",
+                "yaglama", "kokorec", "arabAşi", "waffle", "güveç", "trileçe"));
 
         System.out.println("\n   ***   ");
-        alfBykTekrrsz(menü);
+        alfBykTekrrsz(menu);
         System.out.println("\n   ***   ");
-        chrSayisiTersSirali(menü);
+        chrSayisiTersSirali(menu);
         System.out.println("\n   ***   ");
-        chrSayisiBkSirala(menü);
+        chrSayisiBkSirala(menu);
         System.out.println("\n   ***   ");
-        sonHrfBkSirala(menü);
+        sonHrfBkSirala(menu);
+        System.out.println("\n   ***   ");
+        charKaresiCiftElSirala(menu);
+        System.out.println("\n   ***   ");
+        harfSayisi7denAzKontrol(menu);
+        System.out.println("\n   ***   ");
+        wILeBaslayanElKontrol(menu);
+        System.out.println("\n   ***   ");
+        xILeBitenElKontrol(menu);
+        System.out.println("\n   ***   ");
+        charSayisiBykElPrint(menu);
+
+
     }
 
     // Task : List elemanlarini alafabetik buyuk harf ve  tekrarsiz print ediniz.
@@ -40,8 +49,8 @@ public class Lambda03 {
     }
 
     // Task : list elelmanlarinin character sayisini ters sirali olarak tekrarsiz print ediniz..
-    public static void chrSayisiTersSirali(List<String> menü){
-        menü.
+    public static void chrSayisiTersSirali(List<String> menu){
+        menu.
                 stream().//akısa alındı
                 // map(t->t.length()).
                         map(String::length).//elemanlar karakter sayısına update edildi
@@ -53,16 +62,16 @@ public class Lambda03 {
 
     }
     // Task : List elemanlarini character sayisina gore kckten byk e gore print ediniz..
-    public static void chrSayisiBkSirala(List<String> menü){
-        menü.
+    public static void chrSayisiBkSirala(List<String> menu){
+        menu.
                 stream().
                 sorted(Comparator.
                         comparing(String::length)).
                 forEach(t->System.out.print(t+" "));
     }
     // Task : list elemanlarinin son harfine gore ters sirali print ediniz.
-    public static void sonHrfBkSirala(List<String> menü){
-        menü.
+    public static void sonHrfBkSirala(List<String> menu){
+        menu.
                 stream().
                 sorted(Comparator.
                         comparing(t->t.toString().
@@ -72,10 +81,76 @@ public class Lambda03 {
     }
 
     // Task : listin elemanlarin karakterlerinin cift sayili  karelerini hesaplayan,ve karelerini tekrarsiz buyukten kucuge sirali  print ediniz..
+
+    public static void charKaresiCiftElSirala(List<String>menu){
+        menu.
+                stream().//akısa alndı
+                map(t->t.length()*t.length()).//akısdaki string elemanları boyutlarının karesine update edildi
+                filter(Lambda01::CiftBul).//cift elelmalar filtrelendi
+                distinct().//tekrarsız yapıldı
+                sorted(Comparator.reverseOrder()).//ters b->k sıra yapıldı
+                forEach(Lambda01::yazdir);// print edildi
+
+
+    }
     // Task : List elelmmalarinin karakter sayisini 7 ve 7 'den az olma durumunu kontrol ediniz.
+
+    public static void harfSayisi7denAzKontrol(List<String>menu){
+        //amele kod
+        System.out.println("amele code");
+        boolean kontrol = menu.stream().allMatch(t->t.length()<=7);
+       if (kontrol){
+           System.out.println("List elemanlari 7 ve daha az harften olusuyor");
+       }else {
+           System.out.println("list elemanlari  7 harften buyuk ");
+
+           //System.out.println(kontrol);
+       }
+       // cincix code
+        System.out.println(" cincix code");
+
+        System.out.println(menu.stream().allMatch(t -> t.length() <= 7) ? "List elemanlari 7 ve daha az harften olusuyor" : "list elemanlari  7 harften buyuk ");
+    }
+//anyMatch() --> enaz bir eleman sarti saglarsa true aksi durumda false return eder
+//allMatch() --> tum  elemanlar sarti saglarsa true en az bir eleman sarti saglamazsa false return eder.
+//noneMatch() --> hic bir sarti SAGLAMAZSA true en az bir eleman sarti SAGLARSA false return eder.
+
+
     // Task : List elelmanlarinin "W" ile baslamasını kontrol ediniz.
+    public static void wILeBaslayanElKontrol(List<String> menu){
+        System.out.println(menu.
+                stream().
+
+                noneMatch(t -> t.startsWith("w")) ? " w ile baslaya yemegin menude isi ne" : "w ile baslayan yemek yok tatli vr");
+    }
+
+
+
     // Task : List elelmanlarinin "x" ile biten en az bir elemaı kontrol ediniz.
+
+    public static void xILeBitenElKontrol(List<String> menu){
+        System.out.println(menu.
+                stream().
+                anyMatch((t -> t.endsWith("x"))) ? "x ile biten yemegin menude isi ne" : "x ile biten yemek  vr");
+    }
+
+
     // Task : Karakter sayisi en buyuk elemani yazdiriniz.
+
+    public static void charSayisiBykElPrint(List<String>menu){
+        System.out.println(menu.
+                stream().
+                sorted(Comparator.comparing(t -> t.toString().length()).reversed()).
+                findFirst());
+
+        // akıs cıktısını bir veriable assaign edilebilir
+
+        Optional<String > enBykel=menu.
+                stream().
+                sorted(Comparator.comparing(t -> t.toString().length()).reversed()).
+                findFirst();
+    }
+
     // Task : list elemanlarini son harfine göre siralayıp ilk eleman hariç kalan elemanlari print ediniz.
 }
 
